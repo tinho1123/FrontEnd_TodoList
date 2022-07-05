fd70c0b31b9459a42a5c18dd12d4c68211445e9d
tinho1123 <tinhoefaela@gmail.com>
2022-07-04 23:15:11 -0300

    :sparkles: feat: Create a router

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
 import './App.css';
+import { Routes, Route } from 'react-router-dom'
+import {InitialPage, InitialPageUser } from './pages';
 
 function App() {
   return (
     <div className="App">
+      <Routes> 
+        <Route path='/' element={ <InitialPage />} />
+        <Route path='/:id' element={ <InitialPageUser />} />
+      </Routes>
     </div>
   );
 }
---
+++ app/src/pages/InitialPageUser.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const InitialPageUser = () => {
+  return (
+    <div>InitialPageUser</div>
+  )
+}
